029bd9ec85759b78b2fde359079ac24032d6bc6e
hanjinwook <uki5045@gmail.com>
2022-03-06 15:19:10 +0900

    array

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ import React from 'react';
 const App = () => {
 
     // 상태, 함수, 변수, 상수
+
+    const name = ""
+    const number = 0
      const jinwooks = {
          name : "hanjinwook",
          age : 33,
@@ -25,12 +28,52 @@ const App = () => {
          hobby : "programming"
      }
 
+    const foodILike = [
+        {
+            id: 1,
+            name: "Kimchi",
+            image:
+                "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+        },
+        {
+            id: 2,
+            name: "Samgyeopsal",
+            image:
+                "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+        },
+        {
+            id: 3,
+            name: "Bibimbap",
+            image:
+                "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+        },
+        {
+            id: 4,
+            name: "Doncasu",
+            image:
+                "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+        },
+        {
+            id: 5,
+            name: "Kimbap",
+            image:
+                "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
+        }
+    ]; // array(배열) 데이터
+
 
     // 브라우저 화면에서 보여지는 부분
     return (
         <div>
             <h1>{jinwooks.name}</h1>
             <span>{jinwooks.address}</span>
+            {foodILike.map(food => (
+                <div>
+                <h1>{food.name}</h1>
+                <img src={food.image} alt={food.image} />
+                </div>
+            ))}
+
         </div>
     );
 };
