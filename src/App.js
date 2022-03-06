d3b9f4ec5379457b672b7b46f41fdf1e863f37a6
hanjinwook <uki5045@gmail.com>
2022-03-06 14:58:10 +0900

    const example

--- src/App.js
+++ src/App.js
@@ -18,14 +18,31 @@ import React from 'react';
 const App = () => {
 
     // 상태, 함수, 변수, 상수
+     const jinwooks = {
+         name : "hanjinwook",
+         age : 33,
+         address : "inchoen",
+         hobby : "programming"
+     }
 
 
     // 브라우저 화면에서 보여지는 부분
     return (
         <div>
-            <h1>hello world</h1>
+            <h1>{jinwooks.name}</h1>
+            <span>{jinwooks.address}</span>
         </div>
     );
 };
 
 export default App;
+
+
+
+
+// 코드 빌드순서
+// 컴퓨터는 0부터 시작
+// 1. 위에서 아래로 분석
+// 2. =을 기준으로 오른쪽에서 왼쪽으로 치환
+// 3. .은 하위 메소드나 데이터를 호출하는 명령어
+// 4. [], {}, "", 0 이부분은 데이터 자료형
